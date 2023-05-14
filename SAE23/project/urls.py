0927a48ac3be22ac9b7a1e0766d2d936cfdac0a4
Pierre Le Dreff <pierre.le.dreff@etu.univ-poitiers.fr>
2023-05-14 15:58:51 +0200

    Refont page Index + Personnel
    Première version de la nouvelle mise en page d'Index
    
    Affichage des données Personnel dans un tableau

--- SAE23/project/urls.py
+++ SAE23/project/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('liste_personnel/', views.liste_personnel, name='liste_personnel'),
 ]
--- SAE23/project/views.py
+++ SAE23/project/views.py
@@ -10,3 +10,11 @@ def index(request) :
     }
     return render(request, 'templates/index.html', context)
 
+
+def liste_personnel(request) :
+    personnels = Personnel.objects.all()
+
+    context = {
+        'personnels' : personnels,
+    }
+    return render(request, 'templates/liste_personnel.html', context)
